d1488e2311cb2de8fa1d2a7045beebfa5352714c
Marcos Minond <minond.marcos@gmail.com>
2017-12-18 00:33:13 -0700

    trying out separate files

---
+++ image.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+
+	"github.com/golang/freetype/truetype"
+	"github.com/minond/txtimg/font/hackregular"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+const (
+	fontSize   = 64
+	charWidth  = 44
+	charHeight = 60
+)
+
+var (
+	monoFont font.Face
+)
+
+func init() {
+	monoFont = ttf(hackregular.TTF, fontSize)
+}
+
+func ttf(data []byte, size int) font.Face {
+	f, err := truetype.Parse(data)
+
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing TTF: %v", err))
+	}
+
+	return truetype.NewFace(f, &truetype.Options{
+		SubPixelsX: 64,
+		SubPixelsY: 64,
+		Hinting:    font.HintingFull,
+		Size:       float64(size),
+	})
+}
+
+func write(img *image.RGBA, col color.RGBA, x, y int, str string) {
+	point := fixed.Point26_6{
+		fixed.Int26_6(x * 62),
+		fixed.Int26_6(y * 62),
+	}
+
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(col),
+		Dot:  point,
+		Face: monoFont,
+	}
+
+	d.DrawString(str)
+}
+
+func letter(img *image.RGBA, x, y int, lttr string) {
+	write(img, color.RGBA{0x00, 0x00, 0x00, 0xff},
+		x*charWidth, y*charHeight+charHeight, lttr)
+}
+
+func canvas(w, h int) *image.RGBA {
+	return image.NewRGBA(image.Rect(0, 0, w*charWidth-charWidth, h*charHeight-charHeight))
+}
--- main.go
+++ main.go
@@ -1,80 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"image"
-	"image/color"
 	"image/png"
 	"io/ioutil"
 	"os"
 	"strings"
-
-	"github.com/golang/freetype/truetype"
-	"github.com/minond/txtimg/hackregular"
-	"golang.org/x/image/font"
-	"golang.org/x/image/math/fixed"
 )
-
-const (
-	fontSize   = 64
-	charWidth  = 44
-	charHeight = 60
-)
-
-var (
-	monoFont font.Face
-)
-
-func ttf(data []byte, size int) font.Face {
-	f, err := truetype.Parse(data)
-
-	if err != nil {
-		panic(fmt.Sprintf("Error parsing TTF: %v", err))
-	}
-
-	return truetype.NewFace(f, &truetype.Options{
-		SubPixelsX: 64,
-		SubPixelsY: 64,
-		Hinting:    font.HintingFull,
-		Size:       float64(size),
-	})
-}
-
-func write(img *image.RGBA, col color.RGBA, x, y int, str string) {
-	point := fixed.Point26_6{
-		fixed.Int26_6(x * 62),
-		fixed.Int26_6(y * 62),
-	}
-
-	d := &font.Drawer{
-		Dst:  img,
-		Src:  image.NewUniform(col),
-		Dot:  point,
-		Face: monoFont,
-	}
-
-	d.DrawString(str)
-}
-
-func letter(img *image.RGBA, x, y int, lttr string) {
-	write(img, color.RGBA{0x00, 0x00, 0x00, 0xff}, x*charWidth, y*charHeight+charHeight, lttr)
-}
-
-func canvas(w, h int) *image.RGBA {
-	return image.NewRGBA(image.Rect(0, 0, w*charWidth-charWidth, h*charHeight))
-}
-
-func stdio() string {
-	scanner := bufio.NewScanner(os.Stdin)
-	content := ""
-
-	for scanner.Scan() {
-		content += "\n" + scanner.Text()
-	}
-
-	return content
-}
 
 func file(path string) string {
 	content, err := ioutil.ReadFile(path)
@@ -88,12 +20,7 @@ func file(path string) string {
 }
 
 func usage() {
-	fmt.Println("Usage: go run main.go out.png < in.txt")
-	fmt.Println("       go run main.go in.txt out.png")
-}
-
-func init() {
-	monoFont = ttf(hackregular.TTF, fontSize)
+	fmt.Println("Usage: go run main.go in.txt out.png")
 }
 
 func max(a, b int) int {
@@ -111,9 +38,6 @@ func main() {
 	if len(os.Args) > 2 {
 		str = file(os.Args[1])
 		out = os.Args[2]
-	} else if stat, _ := os.Stdin.Stat(); (stat.Mode() & os.ModeCharDevice) == 0 {
-		str = stdio()
-		out = os.Args[1]
 	} else {
 		usage()
 		os.Exit(2)
